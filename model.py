261301ecddd7ce0539375c3c04ceb0d2fe3aaba8
GallantZZ <gallantkrist@gmail.com>
2025-12-01 15:49:56 +0700

    Update model.py

--- model.py
+++ model.py
@@ -35,4 +35,4 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")  # Accuracy: ~0.91
 
 # save the model to disk
-joblib.dump(clf, "knn_model.sav")
+joblib.dump(clf, "rf_model.sav")
